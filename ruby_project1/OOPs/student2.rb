590597880d5d5c00791c8bbc3ce2eb9406e71929
Kunal Hande <your-github-kunalhand30@gmail.com>
2026-05-22 21:39:04 +0530

    using the initialize method the object added successfully

--- ruby_project1/OOPs/student2.rb
+++ ruby_project1/OOPs/student2.rb
@@ -1,34 +1,21 @@
-class Student1
-attr_accessor  :first_name, :last_name, :email
-attr_reader :username
+class Student2
+  attr_accessor :first_name, :last_name, :username, :email, :password
 
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
-
-#  def first_name=(name)
-#    @first_name = name
-#  end  
-
-# def first_name
-#    @first_name
-#  end  
-
-  def to_s
-    "First name: #{first_name}"
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
   end
 
+  def to_s
+    "First name: #{first_name}, Last name: #{last_name}, username: #{@username}, email: #{@email}, password: #{@password}"
+  end
 end
 
-hk = Student.new
-puts hk
-hk.first_name="define"
-hk.last_name="labs"
-hk.email = "define@gmail.com"
+hk = Student2.new("kunal", "hande", "kunal1", "kunal@gmail.com", "password1")
+rk = Student2.new("rushi", "karpe", "rushi1", "rushi@gmail.com", "password2")
 
-puts hk.first_name
-puts hk.last_name
-puts hk.email
-puts hk.username
+puts hk
+puts rk
