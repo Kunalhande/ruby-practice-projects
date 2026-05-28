9bae18fc9870d0d897b5ee8e9a71b84469c82278
Kunal Hande <your-github-kunalhand30@gmail.com>
2026-05-29 00:07:15 +0530

    new route creation

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root 'application#hello'
+  root 'pages#hello'
+
+  get '/about' to: 'pages#about'
 end
 
